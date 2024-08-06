38f9d2141843bc5c47877bceab8ee7b1869c938f
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-06 19:16:43 +0200

    npm install firebase and add firebase backend configure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello from index.js");
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC085s4No8OQrBznImJZjEUNQm4r5IvnxI",
+  authDomain: "fir-9-tutorial-a2da1.firebaseapp.com",
+  projectId: "fir-9-tutorial-a2da1",
+  storageBucket: "fir-9-tutorial-a2da1.appspot.com",
+  messagingSenderId: "366852303598",
+  appId: "1:366852303598:web:b839b4724af7b8482861ff",
+};
+
+initializeApp(firebaseConfig);
